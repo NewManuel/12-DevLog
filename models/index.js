084a0d2ddba5ce96e601caa7ed0bccad7a4293c1
NewManuel <100541053+NewManuel@users.noreply.github.com>
2024-04-22 21:49:11 -0500

    File - pulling the user, blogspot, and comment s and writing logic for entries then  exporting

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require("./User");
+const Comment = require("./Comment");
+const BlogPost = require("./blogpost");
+
+// users have many recipes
+User.hasMany(BlogPost, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+});
+BlogPost.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+// users have many comments
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+});
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+
+// recipes have many comments
+BlogPost.hasMany(Comment, {
+    foreignKey: "id",
+    onDelete: "CASCADE",
+});
+Comment.belongsTo(BlogPost, {
+    foreignKey: "id",
+});
+
+module.exports = { User, Comment, BlogPost };
